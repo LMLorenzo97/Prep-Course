0f61d2897a4a9e572dd671913d594ffd65deb2f2
Lucas Lorenzo <7nphaser@gmail.com>
2022-05-10 15:54:20 -0300

    prueba

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -174,7 +174,11 @@ function empiezaConNueve(n) {
   //Desarrolle una función que recibe como parámetro un número entero n. Debe retornar true si el entero 
   //inicia con 9 y false en otro caso.
   //Escribe tu código aquí
-  
+  let num = n.toString()
+  if(num.charAt(0) === "9"){
+    return true
+  }
+  return false
 }
 
 
@@ -182,7 +186,12 @@ function todosIguales(arreglo) {
   //Escriba la función todosIguales, que indique si todos los elementos de un arreglo son iguales:
   //retornar true, caso contrario retornar false.
   //Escribe tu código aquí  
-  
+  for (var i =0 ; i < arreglo.length - 1; i++) {
+    if(arreglo[i] !== arreglo[i+1]){
+      return false
+    }
+  }
+  return true
 } 
 
 
@@ -191,6 +200,17 @@ function mesesDelAño(array) {
   // "Enero", "Marzo" y "Noviembre", guardarlo en nuevo array y retornarlo.
   //Si alguno de los meses no está, devolver: "No se encontraron los meses pedidos"
   // Tu código:
+  var mesBuscado = [];
+  for (var i = 0; i < array.length; i++) {
+    if (array[i] === Enero || array[i] === Marzo || array[i] === Noviembre) {
+      mesBuscado.push(array[i]);
+    } 
+  }
+  if (mesBuscado.length < 3) {
+    return "No se encontraron los meses pedidos";
+  } else {
+      return mesBuscado
+  }
 }
 
 
